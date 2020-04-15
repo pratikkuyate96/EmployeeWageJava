54b05913e00b9759df60ae1e25d982dac5c0d73f
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-15 18:49:56 +0530

    ADDED: Part time and full time

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,23 +1,29 @@
-public class EmpWageBuilder {
-
-	public static void main(String[] args) {
-	//Constants
-	int IS_FULL_TIME = 1;
-	int EMP_RATE_PER_HOUR = 20;
-	//Variable
-	int empHrs = 0;
-	int empWage = 0;
-	//Computation
-	double empCheck = Math.floor(Math.random() * 10) % 2;
-	if (empCheck == IS_FULL_TIME) {
-		System.out.println("Employee is Present");
-		empHrs = 8;
-	}
-	else {
-		System.out.println("Employee is Absent");
-		empHrs = 0;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage :" + empWage);
-	}
+class EmpWageBuilder {
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Employee Wage Computation");
+		//CONSTANTS
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HRS=20;
+		int IS_PART_TIME=0;
+		
+		//variables
+		int empHrs=0;
+		int empWage=0;
+		int empCheck=((int)Math.floor(Math.random()*10))%2; 
+		if(empCheck==IS_FULL_TIME) 
+		{
+			empHrs=8;
+		}
+		else if(empCheck==IS_PART_TIME)
+		{
+			empHrs=4;
+		}
+		else
+		{
+			empHrs=0;
+		}
+		empWage=empHrs*EMP_RATE_PER_HRS;
+		System.out.println("Emp Wage for "+empHrs+" hrs : "+empWage);
 	}
 }
